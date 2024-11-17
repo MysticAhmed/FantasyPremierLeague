720327f675a16a6bf8c4513c60b5857a1205549a
AhmedForReal <medo.abdelhady2005@gmail.com>
2024-11-16 19:38:21 -0500

    changed logo and name

--- FPL_Predictions.py
+++ FPL_Predictions.py
@@ -4,6 +4,8 @@ from utils import *
 from Kits import *
 import requests
 
+st.set_page_config(page_title= "Fantasy Premier Predictions", page_icon= 'https://cdn-1.webcatalog.io/catalog/fantasy-premier-league/fantasy-premier-league-icon-filled-256.png?v=1675594263665', layout="centered", 
+                   initial_sidebar_state="auto", menu_items=None)
 goalie_future_fixture, defender_future_fixture, midfielder_fixtures_df, forward_fixtures_df = load_data()
 # Create mappings for player_id to name and player_id to position
 response = requests.get("https://fantasy.premierleague.com/api/bootstrap-static/")
