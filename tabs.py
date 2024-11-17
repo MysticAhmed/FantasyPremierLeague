9868fac038e91d0b87b7a3446eeaeb4c7bb607e3
AhmedForReal <medo.abdelhady2005@gmail.com>
2024-11-17 13:24:04 -0500

    f

--- tabs.py
+++ tabs.py
@@ -47,9 +47,26 @@ def own_team_predictions(goalie_future_fixture, defender_future_fixture, midfiel
         for player in data['elements']
         if positions[player['element_type']] == "Forward" and player['id'] in set(forward_fixtures_df['player_id'])and player['status'] == 'a'   # Only include available players
     ]
-    
+
     # Add a toggle for dark and light mode
     theme_choice = st.radio("Choose Theme:", ["Dark Mode", "Light Mode"], horizontal=True)
+    # Define color themes
+    theme_colors = {
+        "dark": {
+            "position_color": "yellow",
+            "team_color": "white",
+            "value_color": "lime",
+            "points_color": "pink",
+            "background_color" : "black",
+        },
+        "light": {
+            "position_color": "red",
+            "team_color": "black",
+            "value_color": "green",
+            "points_color": "orange",
+            "background_color" : "white",
+        },
+    }
     # Set the theme based on the user's choice
     current_theme = "dark" if theme_choice == "Dark Mode" else "light"
     current_theme_colors = theme_colors[current_theme]
@@ -109,24 +126,6 @@ def own_team_predictions(goalie_future_fixture, defender_future_fixture, midfiel
     selected_defenders_df = defender_future_fixture[defender_future_fixture['player_id'].isin(selected_defender_ids)]
     selected_midfielders_df = midfielder_fixtures_df[midfielder_fixtures_df['player_id'].isin(selected_midfielder_ids)]
     selected_forwards_df = forward_fixtures_df[forward_fixtures_df['player_id'].isin(selected_forward_ids)]
-
-    # Define color themes
-    theme_colors = {
-        "dark": {
-            "position_color": "yellow",
-            "team_color": "white",
-            "value_color": "lime",
-            "points_color": "pink",
-            "background_color" : "black",
-        },
-        "light": {
-            "position_color": "red",
-            "team_color": "black",
-            "value_color": "green",
-            "points_color": "orange",
-            "background_color" : "white",
-        },
-    }
 
     # Function to display a player's information in a column
     def display_player_in_selected_players(column, player):
