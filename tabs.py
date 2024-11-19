17650db0d6703ffeca24541978175c50cbe3d553
AhmedForReal <medo.abdelhady2005@gmail.com>
2024-11-19 18:53:52 -0500

    color

--- tabs.py
+++ tabs.py
@@ -33,7 +33,7 @@ def own_team_predictions(goalie_future_fixture, defender_future_fixture, midfiel
 
     page_text = """
     <style>
-    [data-testid="stWidgetLabel"]{
+    [data-testid="stMarkdownContainer"]{
         color : "#FFFFFF"
     }
     </style>
