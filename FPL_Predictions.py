9a1162c870d750e0ef0672413148b5ebf131f63c
AhmedForReal <medo.abdelhady2005@gmail.com>
2024-11-19 20:33:13 -0500

    color

--- FPL_Predictions.py
+++ FPL_Predictions.py
@@ -51,13 +51,12 @@ if 'formation' not in st.session_state:
 
     #bui396val-1
     {
-        background-color = rgb(38, 39, 48) !important
+        background-color : rgb(38, 39, 48) !important
     }
 
     .st-emotion-cache-1ppef92:hover, .st-emotion-cache-1ppef92:active, .st-emotion-cache-1ppef92:focus-visible
     {
-        background-color = rgb(38, 39, 48) !important
-        padding : 0px
+        background-color : rgb(38, 39, 48) !important
     }
     </style>
     """,
